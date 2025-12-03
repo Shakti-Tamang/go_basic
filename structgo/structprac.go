577feaccec73e0f9185ffc27b9c068df127eef3d
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-12-03 14:53:59 +0545

    feat: struct uses

--- structgo/structprac.go
+++ structgo/structprac.go
@@ -19,8 +19,40 @@ type Person struct {
 
 func StructSimple() {
 
+	var r []Person
+
+	var p Person
+
+	p = Person{"mau", 12, 12.3, "wrking as designer", true}
+
+	p.name = "ram"
+
+	fmt.Println(p.name)
+
+	addresspointer := new(Person)
+
+	fmt.Println("pointer address", addresspointer)
+
 	first := Person{"shakti", 12, 13.4, "working as front end developer", false}
 
+	fmt.Println("age is" + first.job)
 	fmt.Println(first)
 
+	last := Person{name: "sita", age: 12, salary: 12.12, job: "he  is figma designer", maritialstatus: true}
+
+	fmt.Println(last)
+
+	laststruc := &last
+	fmt.Println("string ", laststruc)
+
+	r = append(r, p, first, last)
+
+	for _per := range r {
+
+		fmt.Println("all ages", r[_per].age)
+
+	}
+
 }
+
+// Functions can be associated with a struct type, becoming "methods" of that struct. This allows structs to have behavior in addition to data.
